179e0e015cdb88178894981f93769fd6e80a69cf
erupturatis <bb.octav@gmail.com>
2022-12-06 22:27:02 +0200

    sql server basic connection

--- Server/server.js
+++ Server/server.js
@@ -2,82 +2,33 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+const mysql = require('mysql');
 const GitHubStrategy = require('passport-github2').Strategy;
 const app = express();
 
-app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false,
-    // cookie: {
-    //   httpOnly: true,
-    //   secure: false,
-    //   maxAge: 24 * 60 * 60 * 1000,
-    // },
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function (user, done) {
-  done(null, user);
+// create connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Turbosa341@!',
+  // database: 'nodemysql',
 });
 
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
-});
-
-passport.use(
-  new GitHubStrategy(
-    {
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/github/callback',
-    },
-    function (accessToken, refreshToken, profile, done) {
-      // User.findOrCreate({ githubId: profile.id }, function (err, user) {
-      //   return done(err, user);
-      // });
-      return done(null, profile);
-    }
-  )
-);
-
-app.get('/', (req, res) => {
-  console.log(req.sessionID);
-  //console.log(req.session);
-  res.send(['root', req.session, req.body]);
-});
-
-app.get('/account', ensureAuthenticated, function (req, res) {
-  res.send('got into account');
-});
-
-app.get('/login', (req, res) => {
-  res.send('hello world2');
-});
-
-app.get(
-  '/auth/github',
-  passport.authenticate('github', { scope: ['user:email'] })
-);
-
-app.get(
-  '/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function (req, res) {
-    res.redirect('/');
+db.connect((err) => {
+  if (err) {
+    console.log(err);
   }
-);
-
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
 });
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-}
+app.get('/createdb', (req, res) => {
+  let sql = 'CREATE DATABASE nodemysql';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('databse created');
+  });
+});
+
+app.listen('3000', () => {
+  console.log('started server');
+});
